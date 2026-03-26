855dbc27d41e2e768e7b255925fc419ffb78dd02
kyc2006 <s1131223@o365st.pu.edu.tw>
2026-03-26 12:22:34 +0800

    v0

--- web.py
+++ web.py
@@ -10,6 +10,7 @@ def index():
 	link += "<a  href=/me>關於我</a><hr>"
 	link += "<a  href=/welcome?u=kyc&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
 	link += "<a href=/account>網頁表單傳值</a><br>"
+	link += "<a href = /math>次方與根號運算</a><hr>"
 	return link
 
 @app.route("/mis")
@@ -42,6 +43,34 @@ def account():
         return result
     else:
         return render_template("account.html")
+
+@app.route("/math", methods=["GET", "POST"])
+def math():
+    if request.method == "POST":
+        try:
+            # 對應你的 int(input())，從網頁表單取得資料並轉為整數
+            x = int(request.form["x"])
+            y = int(request.form["y"])
+            opt = request.form["opt"]
+
+            # 你的運算邏輯 (已修正 y=0 的 bug)
+            if opt == "∧":
+                result = x ** y
+            elif opt == "√":
+                if y == 0:
+                    result = "數學不能開0次方根"
+                else:
+                    result = x ** (1/y)
+            else:
+                result = "請輸入 ∧ 或 √"
+            
+            # 將結果顯示在網頁上
+            return f"計算結果：{result} <br><br><a href='/math'>繼續計算</a> | <a href='/'>回首頁</a>"
+            
+        except ValueError:
+            return "輸入格式錯誤，請確保 x 和 y 都是輸入整數！<br><a href='/math'>返回重新計算</a>"
+    else:
+        return render_template("math.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
