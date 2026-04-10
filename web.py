766b211fdd56a578e35f11c784ced66c44bde59d
kyc2006 <s1131223@o365st.pu.edu.tw>
2026-04-10 19:58:24 +0800

    FIREBASE_CONFIG

--- web.py
+++ web.py
@@ -1,21 +1,52 @@
+import random
 from flask import Flask, render_template, request
 from datetime import datetime
+
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# 判斷是在 Vercel 還是本地
+if os.path.exists('firestore/serviceAccountKey.json'):
+    # 本地環境：讀取檔案
+    cred = credentials.Certificate('firestore/serviceAccountKey.json')
+else:
+    # 雲端環境：從環境變數讀取 JSON 字串
+    firebase_config = os.getenv('FIREBASE_CONFIG')
+    cred_dict = json.loads(firebase_config)
+    cred = credentials.Certificate(cred_dict)
+
+firebase_admin.initialize_app(cred)
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	link = "<h1>歡迎進入郭晏丞的網站</h1>"
-	link += "<a  href=/mis>課程</a><hr>"
-	link += "<a  href=/today>現在日期時間</a><hr>"
-	link += "<a  href=/me>關於我</a><hr>"
-	link += "<a  href=/welcome?u=kyc&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
-	link += "<a href=/account>網頁表單傳值</a><br>"
-	link += "<a href = /math>次方與根號運算</a><hr>"
-	return link
+    link = "<h1>歡迎進入郭晏丞的網站</h1>"
+    link += "<a href=/mis>課程</a><hr>"
+    link += "<a href=/today>現在日期時間</a><hr>"
+    link += "<a href=/me>關於我</a><hr>"
+    link += "<a href=/welcome?u=kyc&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
+    link += "<a href=/account>網頁表單傳值</a><hr>"
+    link += "<a href=/math>次方與根號運算</a><hr>"
+    link += "<a href=/cup>線上擲筊</a><hr>"
+    link += "<a href=/read3>讀取Firestore資料</a><hr>"
+    return link
+
+@app.route("/read3")
+def read3():
+    Result = ""
+    db = firestore.client()
+    collection_ref = db.collection("靜宜資管")    
+    docs = collection_ref.order_by("lab", direction=firestore.Query.DESCENDING).get() 
+    for doc in docs:         
+        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
+    return Result
 
 @app.route("/mis")
 def course():
-	return "<h1>資訊管理導論</h1><a href =/>返回網頁</a>"
+	return "<h1>資訊管理導論</h1><a href=/>返回網頁</a>"
 
 @app.route("/today")
 def today():
@@ -71,6 +102,34 @@ def math():
             return "輸入格式錯誤，請確保 x 和 y 都是輸入整數！<br><a href='/math'>返回重新計算</a>"
     else:
         return render_template("math.html")
+
+@app.route('/cup', methods=["GET"])
+def cup():
+    # 檢查網址是否有 ?action=toss
+    #action = request.args.get('action')
+    action = request.values.get("action")
+    result = None
+    
+    if action == 'toss':
+        # 0 代表陽面，1 代表陰面
+        x1 = random.randint(0, 1)
+        x2 = random.randint(0, 1)
+        
+        # 判斷結果文字
+        if x1 != x2:
+            msg = "聖筊：表示神明允許、同意，或行事會順利。"
+        elif x1 == 0:
+            msg = "笑筊：表示神明一笑、不解，或者考慮中，行事狀況不明。"
+        else:
+            msg = "陰筊：表示神明否定、憤怒，或者不宜行事。"
+            
+        result = {
+            "cup1": "/static/" + str(x1) + ".jpg",
+            "cup2": "/static/" + str(x2) + ".jpg",
+            "message": msg
+        }
+        
+    return render_template('cup.html', result=result)
 
 if __name__ == "__main__":
 	app.run(debug=True)
