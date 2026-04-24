ed5b4b4a74ad4aedeb1f1758de92a0eb6d0d0de4
kyc2006 <s1131223@o365st.pu.edu.tw>
2026-04-24 14:11:08 +0800

    爬取即將上映電影

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 import random
+from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 from datetime import datetime
 
@@ -6,6 +7,7 @@ import os
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
+import requests
 
 # 判斷是在 Vercel 還是本地
 if os.path.exists('serviceAccountKey.json'):
@@ -34,7 +36,72 @@ def index():
     link += "<a href=/read3>讀取Firestore資料</a><hr>"
     link += "<a href=/search>搜尋老師</a><hr>"
     link += "<a href=/spider1>爬蟲課程</a><hr>"
+    link += "<a href='/movie'>查詢即將上映電影</a><hr>"
     return link
+
+@app.route("/movie", methods=["GET", "POST"])
+def movie():
+    db = firestore.client()
+
+    # :point_right: 第一次進來（GET）→ 自動爬蟲
+    if request.method == "GET":
+        url = "http://www.atmovies.com.tw/movie/next/"
+        Data = requests.get(url)
+        Data.encoding = "utf-8"
+
+        sp = BeautifulSoup(Data.text, "html.parser")
+        result = sp.select(".filmListAllX li")
+
+        for item in result:
+            try:
+                picture = item.find("img").get("src").strip()
+
+                # :star: 關鍵修正：補完整網址
+                if picture.startswith("/"):
+                    picture = "http://www.atmovies.com.tw" + picture
+
+                title = item.find("div", class_="filmtitle").text.strip()
+
+                link = item.find("a").get("href")
+                movie_id = link.replace("/", "").replace("movie", "")
+
+                hyperlink = "http://www.atmovies.com.tw" + link
+
+                show = item.find("div", class_="runtime").text
+                show = show.replace("上映日期：", "").replace("片長：", "").replace("分", "")
+
+                showDate = show[0:10]
+                showLength = show[13:]
+
+                doc = {
+                    "title": title,
+                    "picture": picture,
+                    "hyperlink": hyperlink,
+                    "showDate": showDate,
+                    "showLength": showLength
+                }
+
+                db.collection("電影").document(movie_id).set(doc)
+
+            except Exception as e:
+                print("錯誤:", e)
+
+        return render_template("movie.html", movies=None)
+
+    # :point_right: 查詢（POST）
+    else:
+        keyword = request.form["MovieTitle"]
+
+        docs = db.collection("電影").get()
+
+        movies = []
+        for doc in docs:
+            data = doc.to_dict()
+
+            if keyword in data.get("title", ""):
+                movies.append(data)
+
+        return render_template("movie.html", movies=movies)
 
 @app.route("/spider1")
 def spider1():
